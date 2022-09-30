fb8c158ebe175975840cb3b87b448dbd0395173d
jdas <jdas@synopsys.com>
2022-09-30 08:33:18 +0530

    Put the resource layer

---
+++ src/main/java/com/example/passwordvalidator/resource/PasswordValidatorController.java
@@ -0,0 +1,16 @@
+package com.example.passwordvalidator.resource;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PasswordValidatorController {
+
+    @PostMapping(value = "validate", produces = {"application/json"})
+    public ResponseEntity<String> validatePassword(@RequestParam(name = "inputStr", required = false) String inputStr) {
+        return new ResponseEntity<>("SUCCESSFUL_VALIDATION_MSG", HttpStatus.OK);
+    }
+}
